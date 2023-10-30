8e58ab2df94c6fdb2421140b0f57ad5583cf14fe
Mahi Garg <mahi7garg@gmail.com>
2023-10-30 07:58:11 +0530

    Initial Commit

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Mahi Garg on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
